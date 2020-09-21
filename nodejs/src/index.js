f6f5d9b01eced24bd47cdf4b5ef47a09c0a034cc
Oskar Thoren <ot@oskarthoren.com>
2020-09-21 12:24:23 +0800

    Import nodejs chat

---
+++ nodejs/src/index.js
@@ -0,0 +1,94 @@
+'use strict'
+
+// Libp2p Core
+const Libp2p = require('libp2p')
+// Transports
+const TCP = require('libp2p-tcp')
+const Websockets = require('libp2p-websockets')
+const WebrtcStar = require('libp2p-webrtc-star')
+const wrtc = require('wrtc')
+// Stream Muxer
+const Mplex = require('libp2p-mplex')
+// Connection Encryption
+const { NOISE } = require('libp2p-noise')
+const Secio = require('libp2p-secio')
+// Chat over Pubsub
+const PubsubChat = require('./chat')
+// Peer Discovery
+const Bootstrap = require('libp2p-bootstrap')
+const MDNS = require('libp2p-mdns')
+const KadDHT = require('libp2p-kad-dht')
+// PubSub implementation
+const Gossipsub = require('libp2p-gossipsub')
+
+;(async () => {
+  // Create the Node
+  const libp2p = await Libp2p.create({
+    addresses: {
+      listen: [
+        '/ip4/0.0.0.0/tcp/0',
+        '/ip4/0.0.0.0/tcp/0/ws',
+        `/ip4/127.0.0.1/tcp/15555/ws/p2p-webrtc-star/`
+      ]
+    },
+    modules: {
+      transport: [ TCP, Websockets, WebrtcStar ],
+      streamMuxer: [ Mplex ],
+      connEncryption: [ NOISE, Secio ],
+      peerDiscovery: [ Bootstrap, MDNS ],
+      dht: KadDHT,
+      pubsub: Gossipsub
+    },
+    config: {
+      transport : {
+        [WebrtcStar.prototype[Symbol.toStringTag]]: {
+          wrtc
+        }
+      },
+      peerDiscovery: {
+        bootstrap: {
+          list: [ '/ip4/127.0.0.1/tcp/63785/ipfs/QmWjz6xb8v9K4KnYEwP5Yk75k5mMBCehzWFLCvvQpYxF3d' ]
+        }
+      },
+      dht: {
+        enabled: true,
+        randomWalk: {
+          enabled: true
+        }
+      }
+    }
+  })
+
+  // Listen on libp2p for `peer:connect` and log the provided connection.remotePeer.toB58String() peer id string.
+  libp2p.connectionManager.on('peer:connect', (connection) => {
+    console.info(`Connected to ${connection.remotePeer.toB58String()}!`)
+  })
+
+    // Start libp2p
+  await libp2p.start()
+
+  // Create our PubsubChat client
+  const pubsubChat = new PubsubChat(libp2p, PubsubChat.TOPIC, ({ from, message }) => {
+    let fromMe = from === libp2p.peerId.toB58String()
+    let user = from.substring(0, 6)
+    if (pubsubChat.userHandles.has(from)) {
+      user = pubsubChat.userHandles.get(from)
+    }
+    console.info(`${fromMe ? PubsubChat.CLEARLINE : ''}${user}(${new Date(message.created).toLocaleTimeString()}): ${message.data}`)
+  })
+
+  // Set up our input handler
+  process.stdin.on('data', async (message) => {
+    // Remove trailing newline
+    message = message.slice(0, -1)
+    // If there was a command, exit early
+    if (pubsubChat.checkCommand(message)) return
+
+    try {
+      // Publish the message
+      await pubsubChat.send(message)
+    } catch (err) {
+      console.error('Could not publish chat', err)
+    }
+  })
+})()
